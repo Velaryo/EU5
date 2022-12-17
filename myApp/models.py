9ea71f0a1cf20aaad864e4786e91b2e3ca2b89ad
Alvaro <ixml3d@gmail.com>
2022-12-17 01:23:50 -0500

    model Pagos

--- myApp/models.py
+++ myApp/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from users.models import User
 
-# Create your models here.
+class Pago(models.Model):
+
+	SERVICIOS_CHOICES = (
+        ('n', 'Netflix'),
+        ('a', 'Amazon Video'),
+        ('s', 'Star +'),
+		('p', 'Paramount +')
+    )
+
+	servicio = models.CharField(max_length=1, choices=SERVICIOS_CHOICES)
+	monto = models.FloatField(default=0)
+	fecha = models.DateField(auto_now_add=True)
+	user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='users')
+
+	class Meta:
+		db_table = 'pagos'
