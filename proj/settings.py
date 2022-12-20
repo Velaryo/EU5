93b8220f6219a3b4bafb2e9fe6346e4faf920d40
Alvaro <ixml3d@gmail.com>
2022-12-19 23:27:59 -0500

    railway v5

--- proj/settings.py
+++ proj/settings.py
@@ -148,12 +148,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = 'static/'
+#STATIC_URL = 'static/'
 
 #* railway
 CSRF_TRUSTED_ORIGINS = ['https://eu5-huarca-gamero-alvaro.up.railway.app']
 
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'staticfiles')]
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 STATIC_ROOT =os.path.join(BASE_DIR, 'staticfiles')
 #* FIN railway
 
